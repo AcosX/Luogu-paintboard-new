5936b2131a77ce2d88dbb029daef21102462c468
Zhangqiyuan <dongfangwawa@outlook.com>
2022-01-01 02:26:27 +0800

    Upload

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # Luogu-paintboard-new
+
 洛谷冬日绘板脚本，采用Token进行绘制。
 
-脚本将在活动开始并经验证可用后上传。
+## 准备
+
+- GIMP
+- Python3 (需安装requests库)
+
+## 使用方法
+
+1. 将图片转为绘板可用的颜色
+   > 引用自 [ouuan/LuoguPaintBoard](https://github.com/ouuan/LuoguPaintBoard)
+   1. 将 `LuoguPaintBoard.gpl` 复制到 `GIMP安装路径\share\gimp\2.0\palettes`。
+   2. 用 GIMP 打开原始图片。
+   3. 图像 → 缩放图像。
+   4. 图像 → 模式 → 索引，Use custom palette：LuoguPaintBoard，**不要** 勾选“从颜色表中移除无用和重复的颜色”。“递色”选项可以自己试试看哪种效果比较好。
+   5. 文件 → 导出为 → 选一个路径 → 选择文件类型：**bmp** → 导出
+   
+   2.将图片转为JSON列表（转换方法源自ouuan的脚本）
+   
+   在data文件夹下执行
+   
+   `python ImageToData.py 图片路径 左上角X坐标 左上角Y坐标`
+   
+   依次对每张图片运行（此脚本为增量更新），完成后可通过 preview.html 查看效果。
+   
+   3.修改tokens.json文件中列表内容为自己的token。
+   
+   4.运行main.py。
+   
+   `python main.py`
+   
+   <br/>
+   
+   
---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+import time
+import threading
+import json
+
+with open('tokens.json','r') as f:
+    tokens = json.load(f)
+with open('data/board.json','r') as f:
+    tasks = json.load(f)
+
+
+token_time = {k: 0 for k in tokens}
+dict_t = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+          'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+server = 'https://www.luogu.com.cn/paintboard'
+boardaddr = server + '/board'
+paintaddr = server + '/paint'
+refer = 'https://www.luogu.com.cn/paintboard'
+cd = 30
+
+# 获取坐标(x,y)上的颜色
+def getBoardColor(x, y):
+    try:
+        return color[x][y]
+    except IndexError:
+        print("Error,",x,y,color)
+# 在(x,y)上用color和token绘画
+def paint(x, y, color, token, refer):
+    data = {'x': x, 'y': y, 'color': color}
+    head = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36','Referer': refer,'content-type':'application/x-www-form-urlencoded'}
+    print(requests.post(paintaddr + "?token=" + token, data=data, headers=head).text,x,y,token)
+
+# 更新绘板颜色数据
+def boardUpdate():
+    global color
+    head = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Referer': refer, 'content-type': 'application/x-www-form-urlencoded'}
+    color = requests.get(boardaddr,headers=head).text.split('\n')
+
+# 获取一个已经冷却的账号
+def getToken():
+    while True:
+        for k, v in token_time.items():
+            if time.time() - v > cd:
+                return k
+
+if __name__ == "__main__":
+    boardUpdate()
+    upd = threading.Thread(target=boardUpdate()) # 创建并开启子线程用于实时更新绘板数据
+    upd.start()
+    while True:
+        for x in tasks: # 循环遍历并绘画
+            if getBoardColor(x[0], x[1]) != str(x[2]):
+                t = getToken()
+                paint(x[0], x[1], x[2], t, refer)
+                token_time[t] = time.time()
